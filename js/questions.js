3fd6ba111ba67b5f71ce70165d806f37166d04ca
Brandon Waller <bwall1331@gmail.com>
2021-02-20 23:55:27 -0500

    added to js and readme

---
+++ js/questions.js
@@ -0,0 +1,85 @@
+// What tag can be used to insert a line break or blank line in an HTML document?
+// body
+// br
+//Title
+
+
+//What tag is used to define a table or image notation (caption)?
+//!DOCTYPE
+//caption
+//code
+//embed
+
+//What tag is used to define the bottom section (footer) of an HTML document?
+//button
+//td
+//h1-h6
+//footer
+
+// What tag is used to define an image – or add an image – to an HTML page?
+// img
+// div
+// meta
+// table
+
+// What tag is used to define a container for an external app or plug-in?
+// caption
+// embed
+// !DOCTYPE
+// code
+
+let questions = [
+    {
+        numb:1,
+        question: "What does HTML stand for?",
+        answer: 'Hyper Text Markup Language',
+        options: [
+            'Hyper Text PreProcessor',           
+            'Hyper Tool Multi Language',
+            'Hyper Tool Multiple Language'
+        ]
+    }
+     {
+        numb:2,
+        question: "What tag is used to define a table or image notation (caption)?",
+        answer: 'caption',
+        options: [
+           '!DOCTYPE',
+            'code',
+            'embed'
+
+        ]
+    }
+    {
+        numb:3,
+        question: 'What tag is used to define the bottom section (footer) of an HTML document?',
+       
+        answer: 'footer',
+        options: [
+            'button',
+            'td',
+            'h1-h6'
+            
+        ]
+    }
+    {
+        numb:4,
+        question: "What tag is used to define an image – or add an image – to an HTML page?",
+        answer: 'img',
+        options: [
+            'div',           
+            'meta',
+            'table'
+        ]
+    }
+    {
+        numb:5,
+        question: "What tag is used to define a container for an external app or plug-in?",
+        answer: 'embed',
+        options: [
+            'caption',           
+            '!DOCTYPE',
+            'code'
+        ]
+    }
+]
---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Dom Elements
+const start_btn = document.querySelector('.start_btn button');
+const info_box = document.querySelector('.info_box');
+const exit_btn = info_box.querySelector('.buttons .quit');
+const continue_btn = info_box.querySelector('.buttons .restart');
+const quiz_box = document.querySelector('.quiz_box');
+
+//Start Button On Click
+// Shows the info box
+start_btn.onclick = ()=>{
+    info_box.classList.add('activeInfo');
+}
+//Exit Button On Click
+// Hides Info Window
+exit_btn.onclick = ()=>{
+    info_box.classList.remove('activeInfo');
+}
+//Continue Button On Click
+//Hides Info box and shows quiz box
+continue_btn.onclick = ()=>{
+    info_box.classList.remove('activeInfo'); //Hide Info Box
+    quiz_box.classList.add('activeQuiz'); //Show Quiz Box
+}
+
+let que_count = 0;
+
+//getting questions and answers form array
+function showQuestions(){
+    const que_text = document.querySelector('.que_text');
+    let que_tag =' <span>'+ questions[0].question +'</span>';
+    que_text.innerHTML - que_tag;
+}
